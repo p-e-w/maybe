a21e85ace8cfed7558d7292db59fe830816c3a3a
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2016-01-30 20:27:11 +0100

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name="maybe",
 
-    version="0.1.0",
+    version="0.2.0",
 
     description="See what a program does before deciding whether you really want it to happen.",
     long_description="For a detailed description, see https://github.com/p-e-w/maybe.",
