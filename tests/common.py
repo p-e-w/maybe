6a8d5c2cab9d7c63295b70d0387beca70ba90b60
Philipp Emanuel Weidmann <pew@worldwidemann.com>
2016-06-18 15:30:40 +0200

    Test with both absolute and relative path arguments

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,7 @@
+import os
 import sys
 import shlex
+from contextlib import contextmanager
 
 from six import PY2, StringIO
 
@@ -22,24 +24,62 @@ def to_unicode(string):
         return string
 
 
+# Source: http://stackoverflow.com/a/431747
+@contextmanager
+def working_directory(directory):
+    original_directory = os.getcwd()
+    try:
+        os.chdir(str(directory))
+        yield
+    finally:
+        os.chdir(original_directory)
+
+
+@contextmanager
+def remove(f):
+    try:
+        yield f
+    finally:
+        if f.check():
+            f.remove()
+            assert not f.check()
+
+
 def tf(directory, command, output, operation, test):
-    def t_file(name):
-        if PY2:
-            name = name.encode(sys.getfilesystemencoding())
-        f = directory.join(name)
+    def t_file(f, f_arg):
         # File does not yet exist (will be created when written to)
         assert not f.check()
         f.write("abc")
         assert f.check()
+        cmd = command.format(f=f_arg)
         # Test for expected output and provided test condition
-        assert maybe("-l -- " + command.format(f=f)) == to_unicode(output.format(f=f))
+        assert maybe("-l -- " + cmd) == to_unicode(output.format(f=f))
         assert test(f)
         # Test for negation of the above if operation is explicitly allowed
-        assert maybe(("-l -a %s -- " % operation) + command.format(f=f)).startswith("maybe has not detected")
+        assert maybe(("-l -a %s -- " % operation) + cmd).startswith("maybe has not detected")
         assert not test(f)
 
-    t_file("filename")
-    # Whitespace in filename
-    t_file("file name")
-    # Unicode in filename
-    t_file(u"file name \u2713")
+    def t_name(name):
+        if PY2:
+            name = name.encode(sys.getfilesystemencoding())
+        # Absolute path
+        with remove(directory.join(name)) as f:
+            t_file(f, str(f))
+        # Relative path
+        with remove(directory.join(name)) as f:
+            t_file(f, name)
+        with remove(directory.mkdir("dirname")) as subdirectory:
+            # Relative path in subdirectory
+            with remove(subdirectory.join(name)) as f:
+                t_file(f, "dirname/" + name)
+            with working_directory(subdirectory):
+                # Relative path in parent directory
+                with remove(directory.join(name)) as f:
+                    t_file(f, "../" + name)
+
+    with working_directory(directory):
+        t_name("filename")
+        # Whitespace in filename
+        t_name("file name")
+        # Unicode in filename
+        t_name(u"file name \u2713")
